568dd2eb2004b96421680f0e1977a35af163d0ff
Tom Westerhout <westerhout@google.com>
2026-07-23 16:47:34 +0000

    add zig config

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,11 +1,13 @@
 local config = { }
 if vim.fn.executable('python') == 1 then
-  config['environment'] = { python = vim.fn.exepath('python') }
+  -- local python = vim.fn.fnamemodify(vim.trim(vim.fn.system({ 'python', '-c', 'import site; print(site.getsitepackages()[0])' })), ':h')
+  local python = vim.fn.exepath('python')
+  config['environment'] = { python = python }
 end
 
 return {
   cmd = { 'ty', 'server' },
   filetypes = { 'python' },
-  root_markers = { 'ty.toml', 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git' },
+  -- root_markers = { 'ty.toml', 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git' },
   settings = { configuration = config },
 }
